a94236df63fce37a69d921c52b244f092b120173
Tim Camise <tcamise@gopro.com>
2024-12-06 15:17:12 -0800

    Bump version

--- capitalizeLib/build.gradle.kts
+++ capitalizeLib/build.gradle.kts
@@ -13,7 +13,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.tcamise-gpsw",
         artifactId = "capitalize",
-        version = "1.0.0"
+        version = "1.0.1"
     )
 
     // Configure POM metadata for the published artifact
